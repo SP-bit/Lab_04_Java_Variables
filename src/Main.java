01b5b0aed8157a3181bd4a30fd8db2e16d87259f
Sathwik <ps.bits5@gmail.com>
2024-09-22 19:51:24 -0400

    Init commit Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int testScore = 95;
+        int marbleCount = 5;
+        int classStudents = 25;
+        int favoriteNumber = 64;
+        int luckyNumber = 99;
+        int height = 77;
+        int lapCount = 33;
+        double gasCost = 4.6;
+        double yogurtPrice = 4.4;
+        double taxRate = 4.55;
+        double itemCost = 34.87;
+        double medicinePrice = 19.3;
+        double serviceFee = 9.23;
+        classStudents = testScore + marbleCount;
+        System.out.println("The sum of " + testScore + " and " + marbleCount + " is equal to " + classStudents);
+        favoriteNumber = testScore * marbleCount;
+        System.out.println("The product of " + testScore + " and " + marbleCount + " is equal to "+ favoriteNumber);
+        luckyNumber = testScore - marbleCount;
+        System.out.println("The difference of " + testScore + " and " + marbleCount + " is equal to "+ luckyNumber);
+        height = testScore / marbleCount;
+        System.out.println("The quotient of " + testScore + " and " + marbleCount + " is equal to "+ height);
+        lapCount = testScore % marbleCount;
+        System.out.println("The remainder of " + testScore + " and " + marbleCount + " is equal to "+ lapCount);
+        taxRate = gasCost + yogurtPrice;
+        System.out.println("The sum of " + gasCost + " and " + yogurtPrice + " is equal to " + taxRate);
+        itemCost = gasCost * yogurtPrice;
+        System.out.println("The product of " + gasCost + " and " + yogurtPrice + " is equal to "+ itemCost);
+        medicinePrice = gasCost - yogurtPrice;
+        System.out.println("The difference of " + gasCost + " and " + yogurtPrice + " is equal to "+ medicinePrice);
+        serviceFee = gasCost / yogurtPrice;
+        System.out.println("The quotient of " + gasCost + " and " + yogurtPrice + " is equal to "+ serviceFee);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
